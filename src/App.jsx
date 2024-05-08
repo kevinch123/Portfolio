48a82dcb61a0f6e25f0cf1d534c3f097924f5cc5
kevinchi <kevinchi78@gmail.com>
2024-05-08 08:48:26 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className="font-roboto flex justify-center items-center w-screen h-screen bg-slate-600">
-      <h1 className="text-3xl text-red-50">hola</h1>
+      <h1 className="text-3xl text-red-50">hola mundo feo</h1>
     
     </main>
   )
